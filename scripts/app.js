ad0e8df0cbbcc58eed28497b01a09f4097314173
LeoJrTheTanooki <aceraccoon@proton.me>
2025-01-10 23:51:06 -0800

    V0.43 - Converted page to single page (redirects to not_found and reads href) Beat joins the footer party!

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,31 @@
 let iframe = document.getElementsByTagName("iframe");
 let tabs = document.getElementById("tabs");
 
+switch (window.location.pathname) {
+  case "/":
+    window.history.pushState("object or string", "Title", "/home");
+    iframe[0].src = "/pages/home.html";
+    break;
+  case "/home":
+    window.history.pushState("object or string", "Title", "/home");
+    iframe[0].src = "/pages/home.html";
+    break;
+  case "/graphicbazaar":
+    window.history.pushState("object or string", "Title", "/graphicbazaar");
+    iframe[0].src = "/pages/graphicbazaar.html";
+    break;
+  case "/roster":
+    window.history.pushState("object or string", "Title", "/roster");
+    iframe[0].src = "/pages/roster.html";
+    break;
+  default:
+    iframe[0].src = "/pages/not_found.html";
+    break;
+}
+
 for (let i = 0; i < tabs.children.length; i++) {
   tabs.children[i].addEventListener("click", (e) => {
     iframe[0].src = e.target.value;
+    window.history.pushState("object or string", "Title", e.target.id);
   });
 }
