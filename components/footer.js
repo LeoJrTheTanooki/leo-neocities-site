b7671873078998d826461bdb9e18efa4c8eb7fad
LeoJrTheTanooki <aceraccoon@proton.me>
2025-07-19 19:09:01 -0700

    Reformatted site to use web components

---
+++ components/footer.js
@@ -0,0 +1,449 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    const dataFetch = async () => {
+      const promise = await fetch("/scripts/chars.json");
+      const data = await promise.json();
+      const charList = data.Characters;
+      return charList;
+    };
+
+    let charListCopy;
+
+    let partyIdxEditor = document.getElementsByClassName("partyIdxEditor");
+
+    for (let i = 0; i < partyIdxEditor.length; i++) {
+      let charList = await dataFetch();
+      partyIdxEditor[i].value = i;
+      partyIdxEditor[i].addEventListener("change", (e) => {
+        if (e.target.value >= charList.length) {
+          e.target.value = 0;
+        } else if (e.target.value < 0) {
+          e.target.value = charList.length - 1;
+        }
+        charListCopy[i] = charList[e.target.value];
+        dataLoad();
+      });
+    }
+
+    const dataLoad = async () => {
+      let charList = await dataFetch();
+      if (!charListCopy) {
+        charListCopy = charList;
+      }
+
+      if (partyIdxEditor.length <= 0) {
+        charListCopy.sort(() => Math.random() - 0.5);
+      }
+
+      let partyMember = document.getElementsByClassName("partyMember");
+
+      for (let i = 0; i < partyMember.length; i++) {
+        let playerDiv = partyMember[i];
+        let playerHealth = playerDiv.children[1];
+        let playerCanvas = playerDiv.children[0].children[0];
+        let playerName =
+          playerHealth.children[0].children[0].children[0].children[0];
+        let playerCanvasCtx = playerCanvas.getContext("2d");
+
+        let listName = charListCopy[i].name;
+        let listSpriteSheet = charListCopy[i].spriteSheet;
+        let listLink = charListCopy[i].link;
+        let listHp = charListCopy[i].hp;
+        let listPp = charListCopy[i].pp;
+        let listWarning = charListCopy[i].warning;
+        let listStatus = charListCopy[i].status;
+
+        let isHovering = false;
+        playerHealth.addEventListener("mouseenter", (e) => {
+          isHovering = true;
+        });
+        playerHealth.addEventListener("mouseleave", (e) => {
+          isHovering = false;
+        });
+
+        if (Array.isArray(listName)) {
+          listName = listName.sort(() => Math.random() - 0.5)[0];
+        }
+        if (Array.isArray(listSpriteSheet)) {
+          listSpriteSheet = listSpriteSheet.sort(() => Math.random() - 0.5)[0];
+        }
+
+        playerName.textContent = listName;
+
+        playerHealth.addEventListener("click", () => {
+          if (listWarning) {
+            if (confirm("WARNING:\n" + listWarning)) {
+              window.open(listLink, "_blank");
+            }
+          } else {
+            window.open(listLink, "_blank");
+          }
+        });
+        playerHealth.title = listLink;
+        let playerHpWheels =
+          playerHealth.children[0].children[0].children[0].children[1]
+            .children[1].children;
+        let playerPpWheels =
+          playerHealth.children[0].children[0].children[0].children[2]
+            .children[1].children;
+        let hpIdx = listHp.length;
+        for (let j = playerHpWheels.length - 1; j >= 0; j--) {
+          hpIdx--;
+          playerHpWheels[j].textContent = listHp[hpIdx];
+          if (listHp[hpIdx]) {
+            playerHpWheels[j].classList.add("scroll-wheel-gradient");
+            playerHpWheels[j].classList.remove("bg-white");
+          }
+        }
+        let ppIdx = listPp.length;
+        for (let j = playerPpWheels.length - 1; j >= 0; j--) {
+          ppIdx--;
+          playerPpWheels[j].textContent = listPp[ppIdx];
+          if (listPp[ppIdx]) {
+            playerPpWheels[j].classList.add("scroll-wheel-gradient");
+            playerPpWheels[j].classList.remove("bg-white");
+          }
+        }
+
+        const loadSheetParam = (src) => {
+          return new Promise((resolve, reject) => {
+            const playerSpriteSheet = new Image();
+            playerSpriteSheet.onload = () => resolve(playerSpriteSheet);
+            playerSpriteSheet.onerror = reject;
+            playerSpriteSheet.src = src;
+            let statusText;
+            listStatus
+              ? (statusText = ", with a " + listStatus + " status effect")
+              : (statusText = "");
+            playerSpriteSheet.alt =
+              "Sprite of " +
+              listName +
+              " rising/dropping behind health container" +
+              statusText;
+          });
+        };
+
+        let spriteSrc = await listSpriteSheet;
+
+        loadSheetParam(spriteSrc).then((spriteSheet) => {
+          const width = 32;
+          const height = 32;
+          const scaledWidth = 32;
+          const scaledHeight = 32;
+          let newLoopCap = spriteSheet.width / 32;
+          let newLoop = newLoopCap;
+          const drawFrame = (frameX, frameY, canvasX, canvasY) => {
+            playerCanvasCtx.drawImage(
+              spriteSheet,
+              frameX * width,
+              frameY * height,
+              width,
+              height,
+              canvasX,
+              canvasY,
+              scaledWidth,
+              scaledHeight
+            );
+          };
+          const step = () => {
+            playerCanvasCtx.clearRect(
+              0,
+              0,
+              playerCanvas.width,
+              playerCanvas.height
+            );
+            drawFrame(newLoop, 0, 0, 0);
+            if (isHovering || window.screen.width < 1024) {
+              newLoop--;
+              if (newLoop <= 0) {
+                newLoop = 0;
+              }
+            } else {
+              newLoop++;
+              if (newLoop >= newLoopCap) {
+                newLoop = newLoopCap - 1;
+              }
+            }
+            window.requestAnimationFrame(step);
+          };
+          window.requestAnimationFrame(step);
+        });
+      }
+    };
+
+    dataLoad();
+
+    this.innerHTML = `
+    <style>
+      body {
+        image-rendering: pixelated;
+      }
+
+      .footer-1 {
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        justify-content: center;
+        position: sticky;
+        margin-top: auto;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+        background: linear-gradient(to top, black 35%, transparent 35%);
+      }
+
+      .flex {
+        display: flex;
+      }
+
+      .justify-center {
+        justify-content: center;
+      }
+
+      .partyMember {
+        align-content: flex-end;
+      }
+
+      .canvas-height {
+        height: 64px;
+      }
+
+      .health-wrap {
+        width: fit-content;
+        font-size: 32px;
+        line-height: 0.954em;
+        cursor: pointer;
+        font-family: AppleKid;
+        font-size: 32px;
+      }
+
+      .health1 {
+        border-radius: 0.4rem;
+        border-width: 0.16rem;
+        border-color: rgb(0, 0, 0);
+        border-style: solid;
+      }
+
+      .health2 {
+        display: flex;
+        width: 7.4rem;
+        flex-direction: column;
+        border-radius: 0.4rem;
+        border-width: 0.16em;
+        border-color: rgb(255, 255, 255);
+        background-color: rgb(224, 192, 160);
+        scrollbar-width: none;
+        background-image: url(/assets/HealthGrid.png);
+        background-size: 12px;
+        image-rendering: pixelated;
+        border-style: solid;
+      }
+
+      .party-member-name {
+        margin-bottom: 3px;
+        text-align: center;
+        line-height: 1.25rem;
+      }
+
+      .points-wrap {
+        margin-bottom: 2px;
+        display: flex;
+        justify-content: space-around;
+        text-align: end;
+        line-height: 12px;
+      }
+
+      .points-text {
+        font-family: SMB3;
+        font-size: 16px;
+        color: rgb(255, 255, 255);
+        text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000,
+          2px 2px 0 #000;
+        color: white;
+      }
+
+      .scroll-wheel-wrap {
+        display: flex;
+        height: 18px;
+        text-align: center;
+      }
+
+      .scroll-wheel {
+        min-width: 12px;
+        border-width: 2px;
+        border-style: solid;
+        border-color: rgb(0, 0, 0);
+        background-color: rgb(255, 255, 255);
+        padding-left: 3px;
+        font-family: BitCell;
+      }
+
+      .scroll-wheel-last {
+        border-right-width: 0px;
+      }
+
+      .scroll-wheel-gradient {
+        background-image: linear-gradient(to top, #e0c0a0, #fff, #e0c0a0);
+      }
+
+      @font-face {
+        font-family: AppleKid;
+        src: url("../assets/woff/Apple-Kid.woff2") format("woff2"),
+          url("../assets/woff/Apple-Kid.woff") format("woff");
+        font-weight: normal;
+        font-style: normal;
+      }
+
+      @font-face {
+        font-family: SMB3;
+        src: url(/assets/woff/Super-Mario-Bros--3.woff);
+      }
+
+      @font-face {
+        font-family: BitCell;
+        src: url(/assets/woff/bitcell_memesbruh03.woff);
+      }
+    </style>
+    <footer class="footer-1">
+      <div class="flex">
+        <div class="partyMember">
+          <div class="flex justify-center">
+            <canvas width="32" height="32" class="canvas-height"> </canvas>
+          </div>
+          <div class="playerHealth">
+            <div class="health-wrap">
+              <div class="health1">
+                <div class="health2">
+                  <div class="party-member-name">&nbsp;</div>
+                  <div class="points-wrap">
+                    <div class="points-text">h p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                  <div class="points-wrap">
+                    <div class="points-text">p p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="partyMember">
+          <div class="flex justify-center">
+            <canvas width="32" height="32" class="canvas-height"> </canvas>
+          </div>
+          <div class="playerHealth">
+            <div class="health-wrap">
+              <div class="health1">
+                <div
+                  class="health2"
+                >
+                  <div class="party-member-name">&nbsp;</div>
+                  <div class="points-wrap">
+                    <div class="points-text">h p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                  <div class="points-wrap">
+                    <div class="points-text">p p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="partyMember">
+          <div class="flex justify-center">
+            <canvas width="32" height="32" class="canvas-height"> </canvas>
+          </div>
+          <div class="playerHealth">
+            <div class="health-wrap">
+              <div class="health1">
+                <div
+                  class="health2"
+                >
+                  <div class="party-member-name">&nbsp;</div>
+                  <div class="points-wrap">
+                    <div class="points-text">h p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                  <div class="points-wrap">
+                    <div class="points-text">p p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="partyMember">
+          <div class="flex justify-center">
+            <canvas width="32" height="32" class="canvas-height"> </canvas>
+          </div>
+          <div class="playerHealth">
+            <div class="health-wrap">
+              <div class="health1">
+                <div
+                  class="health2"
+                >
+                  <div class="party-member-name">&nbsp;</div>
+                  <div class="points-wrap">
+                    <div class="points-text">h p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                  <div class="points-wrap">
+                    <div class="points-text">p p</div>
+                    <div class="scroll-wheel-wrap">
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel scroll-wheel-last"></div>
+                      <div class="scroll-wheel"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    `;
+  }
+}
+
+customElements.define("footer-component", Footer);
---
+++ components/gba.js
@@ -0,0 +1,384 @@
+// create an HTML template element
+const template = document.createElement("template");
+
+template.innerHTML = `
+    
+    <style>
+      body {
+        padding: 0;
+      }
+
+      .btn-depth {
+        filter: drop-shadow(0px 7px 0 black);
+      }
+
+      .pressable:active {
+        background-color: rgb(148, 163, 184);
+        color: rgb(71, 85, 105);
+      }
+
+      .btn-fill {
+        background-color: rgb(226, 232, 240);
+        color: rgb(148, 163, 184);
+      }
+
+      .gba-fill {
+        background-color: rgb(99, 102, 241);
+        color: rgb(55, 48, 163);
+      }
+
+      .gba-wrapper {
+        margin: 1.25rem;
+        display: flex;
+        width: 100%;
+        min-width: 56rem;
+        flex-direction: column;
+        border-radius: 3rem;
+        font-weight: 600;
+        font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        filter: drop-shadow(0rem 2.5rem 0 black);
+      }
+
+      .gba-header {
+        display: flex;
+        justify-content: space-between;
+        border-top-left-radius: 9999px;
+        border-top-right-radius: 9999px;
+      }
+
+      .shoulder-btn {
+        height: 2.25rem;
+        width: 8rem;
+      }
+
+      .shoulder-btn-left {
+        border-bottom-right-radius: 9999px;
+        border-top-left-radius: 9999px;
+      }
+
+      .shoulder-btn-right {
+        border-bottom-left-radius: 9999px;
+        border-top-right-radius: 9999px;
+      }
+
+      .logo-name {
+        width: fit-content;
+        border-radius: 9999px;
+        padding-left: 0.5rem;
+        padding-right: 0.5rem;
+        text-align: center;
+        font-weight: 700;
+        outline-style: solid;
+      }
+
+      .gba-footer {
+        height: 4rem;
+      }
+
+      .gba-main {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      .gba-left {
+        display: flex;
+        flex-direction: column;
+        width: 10rem;
+      }
+
+      .gba-right {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+        width: 10rem;
+      }
+
+      .gba-tabs {
+        margin-top: 0.5rem;
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        font-weight: 400;
+        color: rgb(0, 0, 0);
+      }
+
+      .gba-tab-btn {
+        margin-left: 0.5rem;
+        margin-right: 0.5rem;
+        transform: translate(0, 0) rotate(12deg) skewX(0) skewY(0) scaleX(1)
+          scaleY(1);
+        cursor: pointer;
+        border-radius: 0.5rem;
+        border-width: 0;
+        border-bottom-width: 4px;
+        border-color: rgb(0, 0, 0);
+        background-color: rgb(6, 182, 212);
+        text-align: end;
+        border-style: solid;
+      }
+
+      a.gba-tab-btn {
+        text-decoration: none;
+        color: #000;
+      }
+
+      a.gba-tab-btn:hover {
+        margin-top: 4px;
+        border-bottom-width: 0px;
+        background-color: rgb(103, 232, 249);
+      }
+
+      .gba-dpad-align {
+        margin-top: auto;
+        display: flex;
+        justify-content: center;
+      }
+
+      .gba-dpad-wrapper {
+        display: grid;
+        cursor: default;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        font-size: 1.5rem;
+        line-height: 2rem;
+      }
+
+      .gba-dpad-slot {
+        display: flex;
+        height: 2rem;
+        width: 2rem;
+        align-items: center;
+        justify-content: center;
+      }
+
+
+      .gba-start-wrapper {
+        margin-right: 0.5rem;
+        margin-top: 1.5rem;
+        display: flex;
+        cursor: default;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 0.75rem;
+        padding-bottom: 1rem;
+        font-weight: 600;
+      }
+
+      .gba-start-align {
+        display: flex;
+        width: 5rem;
+        transform: translate(0, 0) rotate(12deg) skewX(0) skewY(0) scaleX(1)
+          scaleY(1);
+        align-items: center;
+        justify-content: space-between;
+        border-radius: 0.75rem;
+        background-color: rgb(79, 70, 229);
+        padding-left: 0.25rem;
+      }
+
+      .gba-start-btn {
+        margin-left: 0.25rem;
+        height: 1rem;
+        width: 1rem;
+        border-radius: 9999px;
+        filter: drop-shadow(0px 3px 0 black);
+      }
+
+      .gba-screen-wrapper {
+        border-radius: 1.5rem;
+        border-width: 16px;
+        border-style: solid;
+        border-color: rgb(38, 38, 38);
+        background-color: rgb(255, 255, 255);
+        height: 65vh;
+        position: relative;
+        overflow: scroll;
+        overscroll-behavior: none;
+        border-bottom-right-radius: 0px;
+        border-bottom-left-radius: 0px;
+        font-weight: 400;
+        color: rgb(0, 0, 0);
+      }
+
+      .gba-brand-wrapper {
+        margin-top: auto;
+        width: 100%;
+        cursor: default;
+        background-color: rgb(38, 38, 38);
+        padding-bottom: 16px;
+        text-align: center;
+        font-weight: 700;
+        font-style: italic;
+        color: rgb(255, 255, 255);
+        text-shadow: -2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000,
+          2px 2px 0 #000;
+        border-radius: 1.5rem /* 24px */;
+        border-top-left-radius: 0px;
+        border-top-right-radius: 0px;
+      }
+
+      .advance-name {
+        font-size: 0.75rem;
+        line-height: 1rem;
+        font-style: normal;
+      }
+
+      .gba-power-wrapper {
+        margin-bottom: auto;
+        display: flex;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+        font-weight: 600;
+      }
+
+      slot {
+        width: 100%;
+      }
+
+      .gba-power-light {
+        height: 0.75rem;
+        width: 0.75rem;
+        border-radius: 9999px;
+        background-color: rgb(163, 230, 53);
+      }
+
+      .gba-ab-wrapper {
+        display: flex;
+        gap: 0.5rem;
+        font-size: 1.875rem;
+        line-height: 2.25rem;
+      }
+
+      .gba-ab-btns {
+        height: 3rem;
+        width: 3rem;
+        cursor: default;
+        border-radius: 9999px;
+        padding-left: 0.5rem;
+        padding-top: 0.5rem;
+        font-weight: 600;
+      }
+
+      .gba-speaker-wrapper {
+        margin-right: 1rem;
+        margin-top: 1.5rem;
+        display: flex;
+        cursor: default;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 0.5rem;
+        padding-bottom: 1rem;
+        font-weight: 600;
+      }
+
+      .gba-speaker {
+        height: 0.25rem;
+        width: 5rem;
+        transform: translate(0, 0) rotate(-12deg) skewX(0) skewY(0) scaleX(1)
+          scaleY(1);
+        border-radius: 0.75rem;
+        background-color: rgb(0, 0, 0);
+      }
+
+      .prevent-select {
+        -webkit-user-select: none; /* Safari */
+        -ms-user-select: none; /* IE 10 and IE 11 */
+        user-select: none; /* Standard syntax */
+        cursor: default;
+      }
+    </style>
+
+    <div class="gba-wrapper gba-fill">
+      <nav class="gba-header">
+        <div class="shoulder-btn shoulder-btn-left btn-fill pressable"></div>
+        <div><p class="logo-name prevent-select">Ace's Blog</p></div>
+        <div class="shoulder-btn shoulder-btn-right btn-fill pressable"></div>
+      </nav>
+      <div class="gba-main">
+        <aside class="gba-left">
+          <div class="gba-tabs" id="tabs">
+            <a class="gba-tab-btn" href="/index.html"> Home </a>
+            <a class="gba-tab-btn" href="/pages/about.html"> About </a>
+            <a class="gba-tab-btn" href="/pages/graphicbazaar.html">
+              Graphics
+            </a>
+            <a class="gba-tab-btn" href="/pages/gallery.html"> Gallery </a>
+            <a class="gba-tab-btn" href="/pages/commissions.html">
+              Commissions
+            </a>
+            <a class="gba-tab-btn" href="/pages/interests.html">
+              Interests
+            </a>
+            <a class="gba-tab-btn" href="/pages/roster.html"> Roster </a>
+          </div>
+          <div class="gba-dpad-align">
+            <div class="gba-dpad-wrapper">
+              <div class="gba-dpad-slot"></div>
+              <div class="gba-dpad-slot btn-fill pressable prevent-select">🞁</div>
+              <div class="gba-dpad-slot"></div>
+              <div class="gba-dpad-slot btn-depth pressable btn-fill prevent-select">
+                🞀
+              </div>
+              <div class="gba-dpad-slot btn-fill prevent-select">●</div>
+              <div class="gba-dpad-slot btn-depth pressable btn-fill prevent-select">
+                🞂
+              </div>
+              <div class="gba-dpad-slot"></div>
+              <div class="gba-dpad-slot btn-depth pressable btn-fill prevent-select">
+                🞃
+              </div>
+            </div>
+          </div>
+          <div class="gba-start-wrapper">
+            <div class="gba-start-align prevent-select">
+              START
+              <div class="gba-start-btn btn-fill pressable"></div>
+            </div>
+            <div class="gba-start-align prevent-select">
+              SELECT
+              <div class="gba-start-btn btn-fill pressable"></div>
+            </div>
+          </div>
+        </aside>
+        <div class="" style="height: 100%; width: 100%">
+          <div class="gba-screen-wrapper"><slot> </slot></div>
+          <div class="gba-brand-wrapper">
+            GAME RACC <span class="advance-name prevent-select">ADVANCE</span>
+          </div>
+        </div>
+        <aside class="gba-right">
+          <div class="gba-power-wrapper">
+            <div class="gba-power-light"></div>
+            <span class="prevent-select">POWER</span>
+          </div>
+          <div class="gba-ab-wrapper">
+            <div style="margin-top: 1rem" class="gba-ab-btns btn-fill pressable btn-depth prevent-select">B</div>
+            <div class="gba-ab-btns btn-fill pressable btn-depth prevent-select">A</div>
+          </div>
+          <div class="gba-speaker-wrapper">
+            <div class="gba-speaker"></div>
+            <div class="gba-speaker"></div>
+            <div class="gba-speaker"></div>
+            <div class="gba-speaker"></div>
+            <div class="gba-speaker"></div>
+          </div>
+        </aside>
+      </div>
+      <div class="gba-footer"></div>
+    </div>
+
+    `;
+
+class Gba extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {}
+}
+customElements.define("gba-component", Gba);
